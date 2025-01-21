eabc40ca25d4d40b69ecec3560e6ca9af66534b8
DobbsDyl <dylandobbs107@gmail.com>
2025-01-20 20:43:43 -0500

    Finished Product Class/Tests
    Product.java is finished, TestProduct.java is also finished.

--- lab1/src/main/java/ca/sheridan/Product.java
+++ lab1/src/main/java/ca/sheridan/Product.java
@@ -13,4 +13,64 @@ public class Product {
         this.quantity = quantity >= 0 ? quantity : 0;
         this.discount = discount >= 0 && discount <= 100 ? discount : 0;
     }
+    
+        //Testing Methods
+    public void sellProduct(int sellAmount) {
+        if (quantity - sellAmount >= 0) 
+            quantity -= sellAmount;
+        else
+            throw new RuntimeException();
+    }
+
+    public void buyProduct(int buyAmount) {
+        if (buyAmount > 0) 
+            quantity += buyAmount;
+        else
+            throw new RuntimeException();
+    }
+
+    public void applyDiscount(double discount) {
+        if (discount >= 0 && discount <= 100) {
+            this.discount = discount / 100;
+            price = Math.abs(price - (price * this.discount));
+        }
+        else
+            throw new RuntimeException();
+        
+    }
+
+    public void removeDiscount() {
+        price = price / (1-discount);
+        discount = 0;
+    }
+
+    //Setters
+    public void setPrice(double newPrice) {
+        if (newPrice >= 0) 
+            price = newPrice;
+        else 
+            throw new RuntimeException();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+    
 }
--- lab1/src/test/java/ca/sheridan/TestProduct.java
+++ lab1/src/test/java/ca/sheridan/TestProduct.java
@@ -1,5 +1,75 @@
 package ca.sheridan;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 public class TestProduct {
-    
+    @Test
+    public void testSellProduct() {
+        Product product = new Product("Book", 30.99, 40, 0);
+        product.sellProduct(10);
+        assertEquals(30, product.getQuantity());
+    }
+
+    @Test
+    public void testBuyProducts() {
+        Product product = new Product("Book", 30.99, 40, 0);
+        product.buyProduct(10);
+        assertEquals(50, product.getQuantity());
+    }
+
+    @Test
+    public void testApplyDiscount() {
+        Product product = new Product("Book", 100, 40, 0);
+        product.applyDiscount(20);
+        assertEquals(0.2, product.getDiscount());
+        assertEquals(80, product.getPrice());
+    }
+
+    @Test
+    public void testRemoveDiscount() {
+        Product product = new Product("Book", 80, 40, 0.2);
+        product.removeDiscount();
+        assertEquals(100, product.getPrice());
+        assertEquals(0, product.getDiscount());
+    }
+
+    @Test
+    public void testSetPrice() {
+        Product product = new Product("Book", 30.99, 40, 0);
+        product.setPrice(40.99);
+        assertEquals(40.99, product.getPrice());
+    }
+
+    @Nested
+    class TestNegativeCases {
+
+        @Test
+        public void testSellProductNegative() {
+            Product product = new Product("Book", 100, 40, 0);
+            assertThrows(RuntimeException.class, ()-> product.sellProduct(50));
+        }
+
+        @Test
+        public void testBuyProductNegative() {
+            Product product = new Product("Book", 100, 40, 0);
+            assertThrows(RuntimeException.class, ()-> product.buyProduct(-10));
+        }
+
+        @Test
+        public void testApplyDiscountNegative() {
+            Product product = new Product("Book", 100, 40, 0);
+            assertThrows(RuntimeException.class, ()-> product.applyDiscount(-10));
+        }
+
+        @Test
+        public void testSetPriceNegative() {
+            Product product = new Product("Book", 100, 40, 0);
+            assertThrows(RuntimeException.class, ()-> product.setPrice(-10));
+        }
+    }
+
 }
