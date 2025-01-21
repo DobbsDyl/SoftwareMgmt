029cbaeb307db11b6878be617787ca80b8857056
DobbsDyl <dylandobbs107@gmail.com>
2025-01-20 20:39:50 -0500

    Added method/fields to product

--- lab1/src/main/java/ca/sheridan/Product.java
+++ lab1/src/main/java/ca/sheridan/Product.java
@@ -1,5 +1,16 @@
 package ca.sheridan;
 
 public class Product {
-    
+    private String name;
+    private double price;
+    private int quantity;
+    private double discount;
+
+    //Constructor
+    public Product(String name, double price, int quantity, double discount) {
+        this.name = name;
+        this.price = price >= 0 ? price : 0;
+        this.quantity = quantity >= 0 ? quantity : 0;
+        this.discount = discount >= 0 && discount <= 100 ? discount : 0;
+    }
 }
