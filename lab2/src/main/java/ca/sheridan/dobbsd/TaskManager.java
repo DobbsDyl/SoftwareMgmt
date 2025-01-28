bb750e30cc2ad05632878061a1af9afe411acd42
DobbsDyl <dylandobbs107@gmail.com>
2025-01-28 15:49:30 -0500

    Deleted from test and added to taskmanager

--- lab2/src/main/java/ca/sheridan/dobbsd/TaskManager.java
+++ lab2/src/main/java/ca/sheridan/dobbsd/TaskManager.java
@@ -1,5 +1,11 @@
 package ca.sheridan.dobbsd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TaskManager {
+    
+    private Map<String, Task> tasks = new HashMap<String, Task>();
+
     
 }
--- lab2/src/test/java/ca/sheridan/dobbsd/TaskManagerTest.java
+++ lab2/src/test/java/ca/sheridan/dobbsd/TaskManagerTest.java
@@ -1,9 +1,6 @@
 package ca.sheridan.dobbsd;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class TaskManagerTest {
     
-    private Map<String, Task> tasks = new HashMap<String, Task>();
+    
 }
