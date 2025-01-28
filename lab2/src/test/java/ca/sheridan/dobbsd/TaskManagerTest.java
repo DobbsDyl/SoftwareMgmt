5c6c2e6dd89794a503dbfaa4886a8364f27313fd
DobbsDyl <dylandobbs107@gmail.com>
2025-01-28 15:43:18 -0500

    Lab 2, added list to taskmanagertest

--- lab2/src/test/java/ca/sheridan/dobbsd/TaskManagerTest.java
+++ lab2/src/test/java/ca/sheridan/dobbsd/TaskManagerTest.java
@@ -1,5 +1,9 @@
 package ca.sheridan.dobbsd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TaskManagerTest {
     
+    private Map<String, Task> tasks = new HashMap<String, Task>();
 }
